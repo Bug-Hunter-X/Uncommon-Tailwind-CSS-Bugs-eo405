da46284f976447cb74d8d1531610bad47e4f241e
BugHunter-X <askzenai@gmail.com>
2025-02-14 03:46:38 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+// This code uses Tailwind classes that don't exist or are misspelled.
+<div class="bg-red-500 p-4 text-white">
+  <p class="text-center">This text is red.</p>
+</div>
+
+// This code uses an outdated Tailwind version and will not work.
+<div class="bg-blue-600">
+  <p class="text-xl">This is a very long text using an outdated tailwind version that is not working.</p>
+</div>
+```
